ea9bc78b3724b7641c9b277add8968eda2c45d88
mnghia0212 <ngonghia0212@gmail.com>
2024-10-17 16:33:38 +0700

    done SR

--- lib/screens/write_mode_study.dart
+++ lib/screens/write_mode_study.dart
@@ -13,15 +13,16 @@ class WriteModeStudy extends ConsumerStatefulWidget {
   const WriteModeStudy({super.key, required this.setId});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _WriteModeStudyState();
+  ConsumerState<WriteModeStudy> createState() => _WriteModeStudyState();
 }
 
 class _WriteModeStudyState extends ConsumerState<WriteModeStudy> {
-  final answerController = TextEditingController();
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController answerController = TextEditingController();
   bool isAnswered = false;
+  bool isCorrect = false;
+  Flashcards? newFlashcard;
 
-  //card boxes
+  // Card boxes
   List<Flashcards> initialBox = [];
   List<Flashcards> wrongBox = [];
   List<Flashcards> firstRightBox = [];
@@ -30,117 +31,100 @@ class _WriteModeStudyState extends ConsumerState<WriteModeStudy> {
   @override
   void initState() {
     super.initState();
+    _initializeFlashcardsBox();
+  }
+
+  void _initializeFlashcardsBox() {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       final flashcardsAsync = ref.read(flashcardStreamProvider(widget.setId!));
       flashcardsAsync.whenData((flashcards) {
-        if (flashcards.isNotEmpty) {
-          initialBox = List<Flashcards>.from(flashcards);
-          ref.read(displayedFlashcardProvider.notifier).state =
-              initialBox[Random().nextInt(initialBox.length)];
-          final card = ref.watch(displayedFlashcardProvider);
-          print("selected: $card");
+        if (flashcards.isNotEmpty && initialBox.isEmpty) {
+          setState(() {
+            initialBox = List<Flashcards>.from(flashcards);
+          });
         }
       });
     });
   }
 
-  Flashcards? showNextFlashcard(bool isCorrect, Flashcards flashcard) {
-    if (initialBox.isEmpty && wrongBox.isEmpty && firstRightBox.isEmpty) {
-      return null;
-    }
+  @override
+  void dispose() {
+    answerController.dispose();
+    super.dispose();
+  }
 
+  Flashcards? _getNextFlashcard(bool isCorrect, Flashcards flashcard) {
+    _updateBoxes(isCorrect, flashcard);
+    return _selectNextFlashcard(flashcard); 
+  }
+
+  void _updateBoxes(bool isCorrect, Flashcards flashcard) {
     if (isCorrect) {
-      if (initialBox.contains(flashcard)) {
-        initialBox.remove(flashcard);
+      if (initialBox.remove(flashcard)) {
         firstRightBox.add(flashcard);
-      } else if (wrongBox.contains(flashcard)) {
-        wrongBox.remove(flashcard);
+      } else if (wrongBox.remove(flashcard)) {
         firstRightBox.add(flashcard);
-      } else if (firstRightBox.contains(flashcard)) {
-        firstRightBox.remove(flashcard);
+      } else if (firstRightBox.remove(flashcard)) {
         secondRightBox.add(flashcard);
       }
     } else {
-      if (initialBox.contains(flashcard)) {
-        initialBox.remove(flashcard);
+      if (initialBox.remove(flashcard)) {
         wrongBox.add(flashcard);
-      } else if (firstRightBox.contains(flashcard)) {
-        firstRightBox.remove(flashcard);
+      } else if (firstRightBox.remove(flashcard)) {
         wrongBox.add(flashcard);
       }
     }
+  }
 
-    debugPrint('initialBox: ${initialBox.toString()}');
-    debugPrint('wrongBox: ${wrongBox.toString()}');
-    debugPrint('firstRightBox: ${firstRightBox.toString()}');
-    debugPrint('secondRightBox: ${secondRightBox.toString()}');
-
-    if (initialBox.isNotEmpty) {
-      return initialBox[Random().nextInt(initialBox.length)];
-    }
-    
-    /*
-    error 1: random lỗi ra số ko có
-    error 2: random displayedCard state = card của set học trước
-    error 3: build formState 2 lần
-     */
-
-    if (wrongBox.isNotEmpty) {
-      if (wrongBox.length == 1 && wrongBox.first == flashcard) {
-        if (firstRightBox.isEmpty) {
-          return wrongBox.first;
-        } else {
-          return firstRightBox[Random().nextInt(firstRightBox.length)];
-        }
-      } else {
-        while (true) {
-          final newfirstWrongBoxCard =
-              wrongBox[Random().nextInt(wrongBox.length)];
-          if (newfirstWrongBoxCard != flashcard) {
-            return newfirstWrongBoxCard;
-          }
-        }
-      }
-    }
-
-    if (firstRightBox.isNotEmpty) {
-      return firstRightBox[Random().nextInt(firstRightBox.length)];
-    }
-
+  Flashcards? _selectNextFlashcard(Flashcards flashcard) {
+    if (initialBox.isNotEmpty) return initialBox[Random().nextInt(initialBox.length)];
+    if (wrongBox.isNotEmpty) return _getRandomFromBox(wrongBox, flashcard);
+    if (firstRightBox.isNotEmpty) return _getRandomFromBox(firstRightBox, flashcard);
     return null;
+  }
+
+  Flashcards? _getRandomFromBox(List<Flashcards> box, Flashcards currentCard) {
+    if (box.length == 1 && box.first == currentCard) {
+      return firstRightBox.isEmpty ? box.first : firstRightBox[Random().nextInt(firstRightBox.length)];
+    }
+
+    Flashcards newCard;
+    do {
+      newCard = box[Random().nextInt(box.length)];
+    } while (newCard == currentCard);
+
+    return newCard;
   }
 
   @override
   Widget build(BuildContext context) {
-    final colors = context.colorScheme;
     final flashcardsAsync = ref.watch(flashcardStreamProvider(widget.setId!));
-    final selectedFlashcard = ref.watch(displayedFlashcardProvider);
+    final colors = context.colorScheme;
     final setName = ref.read(flashcardSetsProvider).selectedFlashcardSet!.title;
 
+    ref.listen<AsyncValue<List<Flashcards>>>(flashcardStreamProvider(widget.setId!), (previous, next) {
+      if (next.hasValue && next.value!.isNotEmpty) {
+        setState(() {
+          initialBox = List<Flashcards>.from(next.value!);
+          ref.read(displayedFlashcardProvider.notifier).state = initialBox[Random().nextInt(initialBox.length)];
+        });
+      }
+    });
+
     return Scaffold(
-      appBar: _appBar(setName),
+      appBar: _buildAppBar(setName),
       body: flashcardsAsync.when(
-        data: (flashcards) {
-          if (flashcards.isEmpty) {
-            return const EmptyContainer(emptyType: EmptyType.card);
-          }
-
-          if (selectedFlashcard == null) {
-            ref.read(displayedFlashcardProvider.notifier).state =
-                initialBox[Random().nextInt(initialBox.length)];
-          }
-
-          return _cardDisplayed(context, ref, colors);
-        },
+        data: (flashcards) => flashcards.isEmpty ? const EmptyContainer(emptyType: EmptyType.card) : _buildCardDisplay(context, colors),
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (error, stackTrace) => Center(child: Text('Error: $error')),
       ),
     );
   }
 
-  Widget _cardDisplayed(
-      BuildContext context, WidgetRef ref, ColorScheme colors) {
-    final selectedFlashcard = ref.watch(displayedFlashcardProvider)!;
+  Widget _buildCardDisplay(BuildContext context, ColorScheme colors) {
+    final selectedFlashcard = ref.watch(displayedFlashcardProvider);
+    if (selectedFlashcard == null) return const Center(child: CircularProgressIndicator());
+
     return Center(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -148,181 +132,104 @@ class _WriteModeStudyState extends ConsumerState<WriteModeStudy> {
           duration: const Duration(milliseconds: 350),
           transitionBuilder: (Widget child, Animation<double> animation) {
             return SlideTransition(
-              position: Tween<Offset>(
-                begin: const Offset(1.0, 0.0),
-                end: Offset.zero,
-              ).animate(animation),
+              position: Tween<Offset>(begin: const Offset(1.0, 0.0), end: Offset.zero).animate(animation),
               child: child,
             );
           },
-          child: _cardContainer(selectedFlashcard, colors, ref),
+          child: _buildCardContainer(selectedFlashcard, colors),
         ),
       ),
     );
   }
 
-  Container _cardContainer(
-      Flashcards selectedFlashcard, ColorScheme colors, WidgetRef ref) {
+  Container _buildCardContainer(Flashcards flashcard, ColorScheme colors) {
     return Container(
-      key: ValueKey(selectedFlashcard.flashcardId),
+      key: ValueKey(flashcard.flashcardId),
       padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 70),
       height: 450,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.25),
-            offset: const Offset(2, 2),
-            blurRadius: 6,
-            spreadRadius: 0,
-          ),
-        ],
+        boxShadow: [BoxShadow(color: Colors.black.withOpacity(0.25), offset: const Offset(2, 2), blurRadius: 6)],
       ),
-      child: _cardContent(selectedFlashcard, colors, ref),
+      child: _buildCardContent(flashcard, colors),
     );
   }
 
-  Form _cardContent(
-      Flashcards selectedFlashcard, ColorScheme colors, WidgetRef ref) {
-    return Form(
-      key: formKey,
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const DisplayText(
-            text: "Câu hỏi / định nghĩa",
-            color: Colors.black,
-            fontSize: 15,
-          ),
-          const Gap(5),
-          DisplayText(
-            text: selectedFlashcard.frontContent,
-            color: Colors.black,
-            fontSize: 19,
-          ),
-          const Gap(20),
-          const DisplayText(
-            text: "Câu trả lời",
-            color: Colors.black,
-            fontSize: 15,
-          ),
-          const Gap(10),
-          _textFieldAnswer(selectedFlashcard),
-          const Spacer(),
-          _rowButtonReaction(colors, ref, selectedFlashcard)
-        ],
-      ),
+  Column _buildCardContent(Flashcards flashcard, ColorScheme colors) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const DisplayText(text: "Câu hỏi / định nghĩa", color: Colors.black, fontSize: 15),
+        const Gap(5),
+        DisplayText(text: flashcard.frontContent, color: Colors.black, fontSize: 19),
+        const Gap(20),
+        const DisplayText(text: "Câu trả lời", color: Colors.black, fontSize: 15),
+        const Gap(10),
+        _buildAnswerTextField(),
+        const Spacer(),
+        _buildActionButtons(colors, flashcard)
+      ],
     );
   }
 
-  TextFormField _textFieldAnswer(Flashcards selectedFlashcard) {
+  TextFormField _buildAnswerTextField() {
     return TextFormField(
-      maxLines: 3,
       controller: answerController,
+      maxLines: 3,
       decoration: InputDecoration(
-          hintText: "Nhập câu trả lời của bạn",
-          enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide: const BorderSide(
-                width: 1,
-                color: Colors.white,
-              )),
-          focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide: BorderSide(
-                width: 1,
-                color: context.colorScheme.primary,
-              )),
-          errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide: const BorderSide(
-                width: 1,
-                color: Colors.red,
-              )),
-          focusedErrorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-              borderSide: const BorderSide(
-                width: 1,
-                color: Colors.red,
-              ))),
-      validator: (value) {
-        if (value!.isEmpty) {
-          return 'Câu trả lời trống';
-        }
-        return null;
-      },
-      onTapOutside: (event) {
-        FocusManager.instance.primaryFocus?.unfocus();
-      },
+        hintText: "Nhập câu trả lời của bạn",
+        enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(15), borderSide: const BorderSide(width: 1, color: Colors.white)),
+        focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(15), borderSide: BorderSide(width: 1, color: context.colorScheme.primary)),
+      ),
+      validator: (value) => value!.isEmpty ? 'Câu trả lời trống' : null,
+      onTapOutside: (event) => FocusManager.instance.primaryFocus?.unfocus(),
     );
   }
 
-  Row _rowButtonReaction(
-      ColorScheme colors, WidgetRef ref, Flashcards selectedFlashcard) {
-    final bool isCorrect = answerController.text.toLowerCase().trim() ==
-        selectedFlashcard.backContent.toLowerCase().trim();
-    Flashcards? newFlashcard;
+  Row _buildActionButtons(ColorScheme colors, Flashcards flashcard) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         ElevatedButton(
           onPressed: () {
-            if (formKey.currentState!.validate()) {
-              setState(() {
-                isAnswered = true;
-                newFlashcard = showNextFlashcard(isCorrect, selectedFlashcard);
-                print("new card 1: $newFlashcard");
-                print("yes/no: $isCorrect");
-                print(
-                    "answer: ${answerController.text} ----- back: ${selectedFlashcard.backContent}");
-              });
-            }
+            setState(() {
+              isAnswered = true;
+              isCorrect = answerController.text.toLowerCase().trim() == flashcard.backContent.toLowerCase().trim();
+              newFlashcard = _getNextFlashcard(isCorrect, flashcard);
+            });
           },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: colors.primary,
-          ),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
-            child: DisplayText(
-              text: "Trả lời",
-            ),
-          ),
+          style: ElevatedButton.styleFrom(backgroundColor: colors.primary),
+          child: const Padding(padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5), child: DisplayText(text: "Trả lời")),
         ),
         const Gap(10),
-        ElevatedButton(
-          onPressed: !isAnswered
-              ? null
-              : () {
-                  print("new card 2: $newFlashcard");
-                  if (newFlashcard != null) {
-                    ref.read(displayedFlashcardProvider.notifier).state =
-                        newFlashcard;
-                    setState(() {
-                      isAnswered = false;
-                      answerController.clear();
-                    });
-                  } else {
-                    debugPrint("SESSION COMPLETED");
-                  }
-                },
-          style: ElevatedButton.styleFrom(
-            backgroundColor: colors.primary,
-          ),
-          child: const Padding(
-              padding: EdgeInsets.symmetric(vertical: 15),
-              child: Icon(
-                Icons.arrow_right_alt,
-                color: Colors.white,
-              )),
-        )
+        _buildNextCardButton(colors)
       ],
     );
   }
 
-  AppBar _appBar(String setName) {
+  ElevatedButton _buildNextCardButton(ColorScheme colors) {
+    return ElevatedButton(
+      onPressed: !isAnswered
+          ? null
+          : () {
+              if (newFlashcard != null) {
+                ref.read(displayedFlashcardProvider.notifier).state = newFlashcard;
+                answerController.clear();
+                setState(() {
+                  isAnswered = false;
+                });
+              } else {
+                debugPrint("SESSION COMPLETED");
+              }
+            },
+      style: ElevatedButton.styleFrom(backgroundColor: colors.primary),
+      child: const Padding(padding: EdgeInsets.symmetric(vertical: 15), child: Icon(Icons.arrow_right_alt, color: Colors.white)),
+    );
+  }
+
+  AppBar _buildAppBar(String setName) {
     return AppBar(title: DisplayTitle(text: "Chế độ viết: $setName"));
   }
 }
